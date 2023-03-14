5b3603e99c178405d77616e3fdea4aba0895e77c
Prosthius <mail@callumhopkins.au>
2023-03-14 19:01:51 +1100

    Changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import Header from "./Header.svelte";
+    import CurrentWeather from "./CurrentWeather.svelte";
 </script>
 
 <Header/>
+<CurrentWeather/>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
