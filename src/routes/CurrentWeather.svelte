da77b5c180b6ec9504e9f1a125dc58ae440d88c1
Prosthius <mail@callumhopkins.au>
2023-03-16 19:59:13 +1100

    Child component of CitySearch, added styling and functionality

--- src/routes/CurrentWeather.svelte
+++ src/routes/CurrentWeather.svelte
@@ -2,23 +2,31 @@
 	import { PUBLIC_API_KEY as apiKey } from '$env/static/public';
 	import { unixToLocale, degreeToCardinal } from '../lib/helper';
 	import { onDestroy, onMount } from 'svelte';
-	import Fab, { Icon } from '@smui/fab';
-	import Textfield from '@smui/textfield';
+	import Cell from '@smui/layout-grid/src/Cell.svelte';
+	import LayoutGrid from '@smui/layout-grid/src/LayoutGrid.svelte';
+	import CircularProgress from '@smui/circular-progress';
+	import InnerGrid from '@smui/layout-grid/src/InnerGrid.svelte';
 	// import * as helper from '$lib';
 
-	let cityName: string = 'Melbourne';
+	let cityNameDefault: string = 'Melbourne';
+	export let cityNameSaved: string;
 	let locationCoords: any;
 	let currentWeather: any;
-    let timeout: any;
+	export let currentWeatherName: string;
+	$: currentWeatherName = currentWeather?.name;
+	let timeout: any;
 
 	onMount(async () => {
-		await getLocation(cityName);
+		await getLocation(cityNameDefault);
 		scheduleWeatherUpdate();
+	});
+
+	onDestroy(() => {
+		clearTimeout(timeout);
 	});
 
 	function scheduleWeatherUpdate() {
 		timeout = setTimeout(async () => {
-			console.log('setInterval()');
 			await getCurrentWeather();
 			scheduleWeatherUpdate();
 		}, 60000);
@@ -30,48 +38,63 @@
 		);
 		let json: any = await res.json();
 		currentWeather = json;
-		console.log(currentWeather);
+		// console.log(currentWeather);
 	}
 
-	async function getLocation(city: string) {
+	export async function getLocation(city: string) {
+		// cityNameSaved = '';
+		currentWeather = '';
 		console.log('getLocation()');
 		let res: any = await fetch(
-			`https://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${apiKey}`
+			`https://api.openweathermap.org/geo/1.0/direct?q=${city.toLowerCase()},AU&limit=1&appid=${apiKey}`
 		);
 		let json: any = await res.json();
 		locationCoords = json[0];
-		console.log(cityName);
 		await getCurrentWeather();
-	}
-
-	function updateCityName() {
-        clearTimeout(timeout);
-		getLocation(cityName);
 	}
 </script>
 
-<Textfield bind:value={cityName} variant="outlined" label="City Name" />
-<Fab>
-	<Icon class="material-icons" on:click={updateCityName}>arrow_forward</Icon>
-</Fab>
-
 {#if currentWeather}
-	<h1>{currentWeather.name}</h1>
-	<h1>Current Temperature: {currentWeather.main.temp.toFixed(1)}&deg;</h1>
-	<h1>Feels Like: {currentWeather.main.feels_like.toFixed(1)}&deg;</h1>
-	<h1>Max: {currentWeather.main.temp_max.toFixed(1)}&deg;</h1>
-	<h1>Min: {currentWeather.main.temp_min.toFixed(1)}&deg;</h1>
-	<h1>Humidity: {currentWeather.main.humidity}%</h1>
+	<LayoutGrid>
+		<Cell>
+			<h3 style="margin-bottom: 0px;">{currentWeather.main.temp.toFixed(1)}&deg;</h3>
+			<InnerGrid>
+				<Cell>
+					<h5>Feels Like</h5>
+				</Cell>
+				<Cell class="centred-vertical" style="max-height: 107px; margin-left: 0px;">
+					<h4 style="transform: translateX(-25px);">
+						{currentWeather.main.feels_like.toFixed(1)}&deg;
+					</h4>
+				</Cell>
+			</InnerGrid>
+		</Cell>
+	</LayoutGrid>
+{:else}
+	<div class="centred-horizontal" style="margin-top: 70px;">
+		<CircularProgress style="height: 100px; width: 100px" indeterminate />
+	</div>
+{/if}
+
+<!-- {#if currentWeather}
+	<h3>{currentWeather.name}:</h3>
+	<h3>Current Temperature: {currentWeather.main.temp.toFixed(1)}&deg;</h3>
+	<h3>Feels Like: {currentWeather.main.feels_like.toFixed(1)}&deg;</h3>
+	<h3>Max: {currentWeather.main.temp_max.toFixed(1)}&deg;</h3>
+	<h3>Min: {currentWeather.main.temp_min.toFixed(1)}&deg;</h3>
+	<h3>Humidity: {currentWeather.main.humidity}%</h3>
 	{#if !currentWeather.wind.speed.toFixed(0)}
-		<h1>Wind: Calm {currentWeather.wind.speed.toFixed(0)}km/h</h1>
+		<h3>Wind: Calm {currentWeather.wind.speed.toFixed(0)}km/h</h3>
 	{:else}
-		<h1>
+		<h3>
 			Wind: {degreeToCardinal(currentWeather.wind.deg)}
 			{currentWeather.wind.speed.toFixed(0)}km/h
-		</h1>
+		</h3>
 	{/if}
-	<h1>Sunrise: {unixToLocale(currentWeather.sys.sunrise)}</h1>
-	<h1>Sunset: {unixToLocale(currentWeather.sys.sunset)}</h1>
+	<h3>Sunrise: {unixToLocale(currentWeather.sys.sunrise)}</h3>
+	<h3>Sunset: {unixToLocale(currentWeather.sys.sunset)}</h3>
 {:else}
-	<h1>Loading...</h1>
-{/if}
+	<h3>Loading...</h3>
+{/if} -->
+<style>
+</style>
