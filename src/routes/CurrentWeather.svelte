e16388c20c9d29196dadd2279785adb8738e8437
Prosthius <mail@callumhopkins.au>
2023-03-26 13:07:27 +1100

    Added location request pop-up, TODO - increase speed, handle rejected request

--- src/routes/CurrentWeather.svelte
+++ src/routes/CurrentWeather.svelte
@@ -6,16 +6,18 @@
 		unixToLocaleTime,
 		formatString
 	} from '../lib/helper';
-	import type { Forecast } from '../lib/interfaces';
+	import type { Forecast } from '../lib/interfaces/Forecast';
+	import type { LocationCoords } from '../lib/interfaces/LocationCoords';
 	import { onDestroy, onMount } from 'svelte';
 	import Cell from '@smui/layout-grid/src/Cell.svelte';
 	import LayoutGrid from '@smui/layout-grid/src/LayoutGrid.svelte';
 	import CircularProgress from '@smui/circular-progress';
 	import InnerGrid from '@smui/layout-grid/src/InnerGrid.svelte';
 	import Paper from '@smui/paper/src/Paper.svelte';
+	import type { Unsubscriber } from 'svelte/store';
 
-	let cityNameDefault: string = 'Warranwood';
-	let locationCoords: any = 'placeholder';
+	let cityNameDefault: string = 'Melbourne';
+	let locationCoords: LocationCoords;
 	export let currentWeather: any;
 	let currentWeatherDescription: string;
 	let currentWeatherName: string;
@@ -27,15 +29,17 @@
 	let hour: number;
 	$: past18Hundred = hour >= 18 ? true : false;
 	let currentForecast: Forecast;
+	let promptPromise: Promise<void> = new Promise<void>((resolve, reject) => {});
 
-	const unsubscribe = forecast.subscribe((value) => {
+	let unsubscribe: Unsubscriber = forecast.subscribe((value: Forecast) => {
 		currentForecast = value;
 	});
-
-	onMount(async () => {
-		await getLocation(cityNameDefault);
+	
+	onMount(() => {
+		promptPromise = promptLocation();
 		scheduleGetWeather();
 		scheduleWeatherUpdate();
+		// await getLocation(cityNameDefault);
 	});
 
 	onDestroy(() => {
@@ -70,7 +74,10 @@
 
 	export async function getLocation(city: string) {
 		try {
-			locationCoords = '';
+			locationCoords = {
+				lat: 0,
+				lon: 0
+			};
 			forecast.set({
 				lat: 0,
 				lon: 0,
@@ -89,101 +96,131 @@
 			console.log(error);
 		}
 	}
+
+	async function promptLocation(): Promise<void> {
+		try {
+			const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+				navigator.geolocation.getCurrentPosition(resolve, reject);
+			});
+			locationCoords = {
+				lat: position.coords.latitude,
+				lon: position.coords.longitude
+			};
+			await getForecast(position.coords.latitude, position.coords.longitude);
+			await getCurrentWeather();
+		} catch (error) {
+			console.error(error);
+			locationCoords = {
+				lat: 0,
+				lon: 0
+			};
+			await getLocation(cityNameDefault);
+			await getForecast(locationCoords.lat, locationCoords.lon);
+			await getCurrentWeather();
+			throw error;
+		}
+	}
 </script>
 
 <Paper style="padding: 1px 20px; margin: 0px 0px 10px 0px;">
 	<h2 class="shadow-text">Current Weather</h2>
 </Paper>
 
-{#if locationCoords === undefined}
-	<h2>Error: Unknown location</h2>
-{:else if currentWeather && forecast}
-	<Paper style="margin-bottom: 10px; padding: 20px 0px;">
-		<LayoutGrid>
-			<Cell spanDevices={{ desktop: 7, tablet: 6, phone: 4 }}>
-				<h2 class="shadow-text current-weather-name" style="margin-top: 0px; whitespace: nowrap;">
-					{currentWeatherName}
-					<h3 class="weather-description">
-						<br />{@html currentWeatherDescription}
-					</h3>
-					<h3 style="margin-bottom: 0px; margin-top: 10px;">
-						<b>{currentWeather.main.temp.toFixed(1)}&deg;</b>
-					</h3>
-				</h2>
-				<InnerGrid>
-					<Cell spanDevices={{ desktop: 7, tablet: 4, phone: 2 }}>
-						<h4>
-							Feels&nbsp;Like <br /> <b>{currentWeather.main.feels_like.toFixed(1)}&deg;</b>
-						</h4>
-					</Cell>
-					{#if past18Hundred}
-						<Cell spanDevices={{ desktop: 4, tablet: 3, phone: 1 }}>
-							<h4>
-								Max Tomorrow <b>{currentForecast?.daily[1].temp.max.toFixed(1)}&deg;</b>
-							</h4>
-						</Cell>
-					{:else}
-						<Cell spanDevices={{ desktop: 4, tablet: 2, phone: 1 }}>
-							<h4>
-								Max <b>{currentForecast?.daily[0].temp.max.toFixed(1)}&deg;</b>
-							</h4>
-						</Cell>
-					{/if}
-					<Cell spanDevices={{ desktop: 1, tablet: 2, phone: 1 }}>
-						<h4>
-							Min <b>{currentForecast?.daily[0].temp.min.toFixed(1)}&deg;</b>
-						</h4>
-					</Cell>
-				</InnerGrid>
-			</Cell>
-		</LayoutGrid>
-	</Paper>
-	<Paper style="margin-bottom: 10px; padding: 20px 0px;">
-		<LayoutGrid>
-			<Cell spanDevices={{ desktop: 6, tablet: 4, phone: 4 }}>
-				{#if windSpeedTrue}
-					<h4>
-						Wind
-						<b>
-							{degreeToCardinal(currentWeather.wind.deg)}&nbsp;{currentWeather.wind.speed.toFixed(
-								0
-							)}km/h
-							<br />
-						</b>
-						{#if currentWeather.wind.gust}
-							Gust&#x202f; <!-- U+202F NARROW NO-BREAK SPACE -->
-							<b>
-								{currentWeather.wind.gust?.toFixed(0)}km/h
-							</b>
-						{/if}
-					</h4>
-				{:else}
-					<h4>Wind <b>Calm</b></h4>
-				{/if}
-			</Cell>
-			<Cell spanDevices={{ desktop: 6, tablet: 4, phone: 4 }}>
-				<h4 style="margin-top: 0px; margin-bottom: 0px;">
-					Pressure <b>{currentWeather.main.pressure}&nbsp;hPa</b>
-					<br />
-					Humidity&nbsp;<b>{currentWeather.main.humidity}%</b>
-				</h4>
-			</Cell>
-		</LayoutGrid>
-		<LayoutGrid>
-			<Cell spanDevices={{ desktop: 12, tablet: 8, phone: 4 }}>
-				<h4>
-					Sunrise <b>{unixToLocaleTime(currentWeather.sys.sunrise)}</b>
-					<br />
-					Sunset&nbsp; <b>{unixToLocaleTime(currentWeather.sys.sunset)}</b>
-				</h4>
-			</Cell>
-		</LayoutGrid>
-	</Paper>
-{:else}
+{#await promptPromise}
 	<div class="centred-horizontal" style="margin-top: 70px;">
 		<CircularProgress style="height: 100px; width: 100px" indeterminate />
 	</div>
-{/if}
+{:then}
+	{#if locationCoords === undefined}
+		<h2>Error: Unknown location</h2>
+	{:else if currentWeather && forecast}
+		<Paper style="margin-bottom: 10px; padding: 20px 0px;">
+			<LayoutGrid>
+				<Cell spanDevices={{ desktop: 7, tablet: 6, phone: 4 }}>
+					<h2 class="shadow-text current-weather-name" style="margin-top: 0px; whitespace: nowrap;">
+						{currentWeatherName}
+						<h3 class="weather-description">
+							<br />{@html currentWeatherDescription}
+						</h3>
+						<h3 style="margin-bottom: 0px; margin-top: 10px;">
+							<b>{currentWeather.main.temp.toFixed(1)}&deg;</b>
+						</h3>
+					</h2>
+					<InnerGrid>
+						<Cell spanDevices={{ desktop: 7, tablet: 4, phone: 2 }}>
+							<h4>
+								Feels&nbsp;Like <br /> <b>{currentWeather.main.feels_like.toFixed(1)}&deg;</b>
+							</h4>
+						</Cell>
+						{#if past18Hundred}
+							<Cell spanDevices={{ desktop: 4, tablet: 3, phone: 1 }}>
+								<h4>
+									Max Tomorrow <b>{currentForecast?.daily[1].temp.max.toFixed(1)}&deg;</b>
+								</h4>
+							</Cell>
+						{:else}
+							<Cell spanDevices={{ desktop: 3, tablet: 2, phone: 1 }}>
+								<h4>
+									Max <b>{currentForecast?.daily[0].temp.max.toFixed(1)}&deg;</b>
+								</h4>
+							</Cell>
+						{/if}
+						<Cell spanDevices={{ desktop: 1, tablet: 2, phone: 1 }}>
+							<h4>
+								Min <b>{currentForecast?.daily[0].temp.min.toFixed(1)}&deg;</b>
+							</h4>
+						</Cell>
+					</InnerGrid>
+				</Cell>
+			</LayoutGrid>
+		</Paper>
+		<Paper style="margin-bottom: 10px; padding: 20px 0px;">
+			<LayoutGrid>
+				<Cell spanDevices={{ desktop: 6, tablet: 4, phone: 4 }}>
+					{#if windSpeedTrue}
+						<h4>
+							Wind
+							<b>
+								{degreeToCardinal(currentWeather.wind.deg)}&nbsp;{currentWeather.wind.speed.toFixed(
+									0
+								)}km/h
+								<br />
+							</b>
+							{#if currentWeather.wind.gust}
+								Gust&#x202f; <!-- U+202F NARROW NO-BREAK SPACE -->
+								<b>
+									{currentWeather.wind.gust?.toFixed(0)}km/h
+								</b>
+							{/if}
+						</h4>
+					{:else}
+						<h4>Wind <b>Calm</b></h4>
+					{/if}
+				</Cell>
+				<Cell spanDevices={{ desktop: 6, tablet: 4, phone: 4 }}>
+					<h4 style="margin-top: 0px; margin-bottom: 0px;">
+						Pressure <b>{currentWeather.main.pressure}&nbsp;hPa</b>
+						<br />
+						Humidity&nbsp;<b>{currentWeather.main.humidity}%</b>
+					</h4>
+				</Cell>
+			</LayoutGrid>
+			<LayoutGrid>
+				<Cell spanDevices={{ desktop: 12, tablet: 8, phone: 4 }}>
+					<h4>
+						Sunrise <b>{unixToLocaleTime(currentWeather.sys.sunrise)}</b>
+						<br />
+						Sunset&nbsp; <b>{unixToLocaleTime(currentWeather.sys.sunset)}</b>
+					</h4>
+				</Cell>
+			</LayoutGrid>
+		</Paper>
+	{:else}
+		<div class="centred-horizontal" style="margin-top: 70px;">
+			<CircularProgress style="height: 100px; width: 100px" indeterminate />
+		</div>
+	{/if}
+{/await}
 
 <style>
 	h4 {
