bdf77255a701657e358d3f613da0d4847f6a8a1c
Prosthius <mail@callumhopkins.au>
2023-03-27 17:24:59 +1100

    Modified getCurrentWEather() to use a writable

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,5 +1,56 @@
 import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 import type { Forecast } from './interfaces/Forecast';
+import type { CurrentWeather } from './interfaces/CurrentWeather';
+
+const mockForecast: Forecast = {
+	lat: 0,
+	lon: 0,
+	timezone: '',
+	timezone_offset: 0,
+	daily: []
+};
+
+export const mockCurrentWeather: CurrentWeather = {
+	coord: {
+		lon: 0,
+		lat: 0,
+	},
+	weather: [{
+		id: 0,
+		main: '',
+		description: '',
+		icon: '',
+	}],
+	base: '',
+	main: {
+		temp: 0,
+		feels_like: 0,
+		temp_min: 0,
+		temp_max: 0,
+		pressure: 0,
+		humidity: 0,
+	},
+	wind: {
+		speed: 0,
+		deg: 0,
+	},
+	clouds: {
+		all: 0,
+	},
+	dt: 0,
+	sys: {
+		type: 0,
+		id: 0,
+		country: '',
+		sunrise: 0,
+		sunset: 0,
+	},
+	timezone: 0,
+	id: 0,
+	name: '',
+	cod: 0,
+};
 
 export function unixToLocaleTime(unixUTC: number): string {
 	let date: Date = new Date(unixUTC * 1000);
@@ -87,13 +138,7 @@ export function formatString(input: string): string {
 	return formatted.trim();
 }
 
-export let forecast = writable<Forecast>({
-	lat: 0,
-	lon: 0,
-	timezone: '',
-	timezone_offset: 0,
-	daily: []
-});
+export let forecast: Writable<Forecast> = writable<Forecast>(mockForecast);
 
 export async function getForecast(lat: number, lon: number): Promise<void> {
 	let res: Response = await fetch(
@@ -101,6 +146,17 @@ export async function getForecast(lat: number, lon: number): Promise<void> {
 	);
 	let json: Forecast = await res.json();
 	forecast.set(json);
+}
+
+export let currentWeather: Writable<CurrentWeather> = writable<CurrentWeather>(mockCurrentWeather);
+
+export async function getCurrentWeather(lat: number, lon: number): Promise<void> {
+	currentWeather.set(mockCurrentWeather);
+	let res: Response = await fetch(
+		`https://current.weather.callumhopkins.au/weather?lat=${lat}&lon=${lon}`
+	);
+	let json: CurrentWeather = await res.json();
+	currentWeather.set(json);
 }
 
 let forecastDummyData: Forecast = {
