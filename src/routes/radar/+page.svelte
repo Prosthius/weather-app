64a04b9259efd5d7fc72c66842e7420a8e9c6394
Prosthius <mail@callumhopkins.au>
2023-03-29 12:52:14 +1100

    Created page with world map and radar overlay

--- src/routes/radar/+page.svelte
+++ src/routes/radar/+page.svelte
@@ -1,1 +1,48 @@
-<h1>radar</h1>
+<script lang="ts">
+	import type { LocationCoords } from '$lib/interfaces/LocationCoords';
+	import { locationCoords } from '$lib/apiCalls';
+	import { PUBLIC_API_KEY as apiKey } from '$env/static/public';
+	import { onMount, onDestroy } from 'svelte';
+	import L from 'leaflet';
+	import type { Unsubscriber } from 'svelte/store';
+
+	const mapLayer: string = 'precipitation_new';
+	const baseUrl: string = 'https://tile.openweathermap.org/map';
+	let mapElement: HTMLDivElement;
+	let locationCoordsLocal: LocationCoords;
+	let locationCoordsUnsubscribe: Unsubscriber = locationCoords.subscribe(
+		(value: LocationCoords) => {
+			locationCoordsLocal = value;
+		}
+	);
+
+	onMount(async () => {
+		console.log(locationCoordsLocal);
+		const map = L.map(mapElement).setView([locationCoordsLocal.lat, locationCoordsLocal.lon], 13);
+
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+
+		L.tileLayer(`${baseUrl}/${mapLayer}/{z}/{x}/{y}/.png?appid=${apiKey}`, {
+			attribution: 'Map data &copy; OpenWeatherMap',
+			maxZoom: 19,
+            opacity: 1
+		}).addTo(map);
+	});
+
+	onDestroy(() => {
+		locationCoordsUnsubscribe();
+	});
+</script>
+
+<div class="border" id="map" bind:this={mapElement} />
+
+<style>
+	@import 'leaflet/dist/leaflet.css';
+	#map {
+		height: 500px;
+		width: 100%;
+	}
+</style>
