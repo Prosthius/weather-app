799d387eb3c9c8d98b5fc75aa255d7ecb0e526c6
Prosthius <mail@callumhopkins.au>
2023-03-29 12:44:24 +1100

    Updated interface

--- src/lib/interfaces/LocationCoords.ts
+++ src/lib/interfaces/LocationCoords.ts
@@ -2,3 +2,14 @@ export interface LocationCoords {
     lat: number;
     lon: number;
 }
+
+// export interface LocationCoords {
+//     name: string;
+//     local_names: {
+//       [language: string]: string;
+//     };
+//     lat: number;
+//     lon: number;
+//     country: string;
+//     state: string;
+//   }
